a75b36a1b534db5d0ef98b5d4612336dfa79c98a
Ali Yıldız <aliyildiz@outlook.com.tr>
2018-07-09 17:01:29 +0300

    Initial commit

---
+++ app/src/main/java/com/aliyildiz/ecosystem_mail/MainActivity.java
@@ -0,0 +1,17 @@
+package com.aliyildiz.ecosystem_mail;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
